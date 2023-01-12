306837ea557cb48a85aded2eb6a4216be3070fa4
goelabhishek694 <goelabhishek694@gmail.com>
2023-01-12 21:57:14 +0530

    :fire: lerant redux toolkit created counter app

--- 10-01-2023/reduxdemo/src/redux/ballReducer.js
+++ 10-01-2023/reduxdemo/src/redux/ballReducer.js
@@ -1,5 +1,5 @@
 const intialState = {
-    balls:50
+    balls:50,
 
 };
 // Reducers are pure functions that take the current state of an application, perform an action, and return a new state. The reducer handles how the state (application data) will change in response to an action.
